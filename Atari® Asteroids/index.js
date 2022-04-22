8f37267ce540c25f5b7826e9489b7acbf9db2b71
Nathan M <epicgamernjm@gmail.com>
2022-04-22 16:28:47 -0500

    atari bit silly

--- Atari® Asteroids/index.js
+++ Atari® Asteroids/index.js
@@ -1,26 +1,34 @@
-var newDeg = 0;
-var x = 0;
-var y = 0;
+var angle = 0;
+var xPoint = 0;
+var yPoint = 0;
+var horzMove = 0;
+var vertMove = 0;
+var hypotenuse = 14.142;
 var spaceship = $(".spaceship");
+// var offsets = $('spaceship').offset();
+// var top = offsets.top;
+// var left = offsets.left;
 
 $(document).keydown(function(){
   switch (event.key){
     case "a":
-        newDeg -= 25.714;
-        spaceship.css({'transform': 'translate(-50%, -50%) rotate('+ newDeg +'deg)' });
+        angle -= 22.5;
+        spaceship.css({'transform': 'translate(-50%, -50%) rotate('+ angle +'deg)' });
+        console.log(spaceship.postion);
         break;
     case "w":
-        spaceship.finish().animate({
-            marginTop: "-=10"
-        });
+        move();
+        spaceship.css({'transform': 'translate('+ horzMove + 'px,'+ vertMove +'px) rotate('+ angle +'deg)'});
+        console.log(spaceship.postion);
         break;
     case "d":
-        newDeg += 25.714;
-      spaceship.css({'transform': 'translate(-50%, -50%) rotate('+ newDeg +'deg)' });
+        angle += 22.5;
+        spaceship.css({'transform': 'translate(-50%, -50%) rotate('+ angle +'deg)' });
+        console.log(spaceship.postion);
         break;
     case "s":
-        spaceship.fadeOut(600);
-        setTimeout(getRandomPos, 600);
+        // spaceship.fadeOut(600);
+        // setTimeout(getRandomPos, 600);
         break;
     }
 });
@@ -28,9 +36,13 @@ $(document).keydown(function(){
 function getRandomPos(){
   let windowW = $(window).width();
   let windowH = $(window).height();
-  console.log(y)
   x = Math.floor(Math.random() * windowW);
   y = Math.floor(Math.random() * windowH);
-  spaceship.fadeIn(600).css('margin-top', x);
-  spaceship.css('margin-left', y);
+  spaceship.fadeIn(600).css('margin-top', xPoint);
+  spaceship.css('margin-left', yPoint);
+}
+
+function move(){
+  horzMove += (Math.cos(angle) * hypotenuse)
+  vertMove += (Math.sin(angle) * hypotenuse)
 }
