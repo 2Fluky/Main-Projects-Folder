aa42e452a0ee043bda4d44e19a8fe7afb44a28f1
Nathan M <epicgamernjm@gmail.com>
2022-04-26 16:31:24 -0500

    Atari better now

--- Atari® Asteroids/index.js
+++ Atari® Asteroids/index.js
@@ -5,6 +5,9 @@ var horzMove = 0;
 var vertMove = 0;
 var hypotenuse = 14.142;
 var spaceship = $(".spaceship");
+var radian = 0.0174533;
+var currX = 0;
+var currY = 0;
 // var offsets = $('spaceship').offset();
 // var top = offsets.top;
 // var left = offsets.left;
@@ -14,17 +17,19 @@ $(document).keydown(function(){
     case "a":
         angle -= 22.5;
         spaceship.css({'transform': 'translate(-50%, -50%) rotate('+ angle +'deg)' });
-        console.log(spaceship.postion);
+        spaceship.css('bottom', currY);
+        spaceship.css('left', currX);
         break;
     case "w":
         move();
         spaceship.css({'transform': 'translate('+ horzMove + 'px,'+ vertMove +'px) rotate('+ angle +'deg)'});
-        console.log(spaceship.postion);
+        getCurrPos();
         break;
     case "d":
         angle += 22.5;
         spaceship.css({'transform': 'translate(-50%, -50%) rotate('+ angle +'deg)' });
-        console.log(spaceship.postion);
+        spaceship.css('top', currY);
+        spaceship.css('left', currX);
         break;
     case "s":
         // spaceship.fadeOut(600);
@@ -43,6 +48,14 @@ function getRandomPos(){
 }
 
 function move(){
-  horzMove += (Math.cos(angle) * hypotenuse)
-  vertMove += (Math.sin(angle) * hypotenuse)
+  let deg = angle * radian;
+  horzMove += (Math.sin(deg) * hypotenuse);
+  vertMove -= (Math.cos(deg) * hypotenuse);
+}
+
+function getCurrPos(){
+let offsets = spaceship.offset();
+currY = (offsets.top + 'px'); // clicking a goes up and to the right
+currX = (offsets.left + 'px'); // clicking d goes down and to the right
+console.log(currY, currX)
 }
