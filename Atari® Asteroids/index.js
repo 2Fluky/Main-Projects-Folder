bdf99ea27de32d1623d27881a3c90b4cda23bb7d
Nathan M <epicgamernjm@gmail.com>
2022-05-02 19:46:08 -0500

    hyperspace fix n updates

--- Atari® Asteroids/index.js
+++ Atari® Asteroids/index.js
@@ -12,37 +12,33 @@ var currY = 0;
 // var top = offsets.top;
 // var left = offsets.left;
 
-$(document).keydown(function(){
+$(document).keypress(function(){
   switch (event.key){
     case "a":
         angle -= 22.5;
         spaceship.css({'transform': 'translate(-50%, -50%) rotate('+ angle +'deg)' });
-        spaceship.css('bottom', currY);
-        spaceship.css('left', currX);
         break;
     case "w":
         move();
         spaceship.css({'transform': 'translate('+ horzMove + 'px,'+ vertMove +'px) rotate('+ angle +'deg)'});
-        getCurrPos();
+        // getCurrPos();
         break;
     case "d":
         angle += 22.5;
         spaceship.css({'transform': 'translate(-50%, -50%) rotate('+ angle +'deg)' });
-        spaceship.css('top', currY);
-        spaceship.css('left', currX);
         break;
     case "s":
-        // spaceship.fadeOut(600);
-        // setTimeout(getRandomPos, 600);
+        spaceship.fadeOut(600);
+        setTimeout(getRandomPos, 600);
         break;
     }
 });
 
 function getRandomPos(){
-  let windowW = $(window).width();
-  let windowH = $(window).height();
-  x = Math.floor(Math.random() * windowW);
-  y = Math.floor(Math.random() * windowH);
+  let windowW = $(window).innerWidth();
+  let windowH = $(window).innerHeight();
+  xPoint = Math.floor(Math.random() * windowW);
+  yPoint = Math.floor(Math.random() * windowH);
   spaceship.fadeIn(600).css('margin-top', xPoint);
   spaceship.css('margin-left', yPoint);
 }
@@ -53,9 +49,9 @@ function move(){
   vertMove -= (Math.cos(deg) * hypotenuse);
 }
 
-function getCurrPos(){
-let offsets = spaceship.offset();
-currY = (offsets.top + 'px'); // clicking a goes up and to the right
-currX = (offsets.left + 'px'); // clicking d goes down and to the right
-console.log(currY, currX)
-}
+// function getCurrPos(){
+// let offsets = spaceship.offset();
+// currY = (offsets.top + 'px');
+// currX = (offsets.left + 'px');
+// console.log(currY, currX)
+// }
