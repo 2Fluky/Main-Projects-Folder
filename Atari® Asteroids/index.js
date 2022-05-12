b8f17cf842a30b9e4cc315cbfe37090407f07d2b
Nathan M <epicgamernjm@gmail.com>
2022-05-12 16:21:07 -0500

    Added Shooting Mechanics(needs work)

--- Atari® Asteroids/index.js
+++ Atari® Asteroids/index.js
@@ -6,8 +6,10 @@ var vertMove = 0;
 var hypotenuse = 14.142;
 var spaceship = $(".spaceship");
 var radian = 0.0174533;
-var currX = 0;
-var currY = 0;
+var spaceshipX = document.querySelector('.spaceship').offsetWidth;
+var spaceshipY = document.querySelector('.spaceship').offsetHeight;
+var rect = document.querySelector('.spaceship').getBoundingClientRect();
+
 // var offsets = $('spaceship').offset();
 // var top = offsets.top;
 // var left = offsets.left;
@@ -16,8 +18,6 @@ $(document).keypress(function(){
   switch (event.key){
       case "a":
         angle -= 22.5;
-        var currX = 0;
-        var currY = 0;
         spaceship.css({'transform': 'translate('+ horzMove + 'px,'+ vertMove +'px) rotate('+ angle +'deg)' });
         break;
     case "w":
@@ -27,16 +27,18 @@ $(document).keypress(function(){
         break;
     case "d":
         angle += 22.5;
-        var currX = 0;
-        var currY = 0;
         spaceship.css({'transform': 'translate('+ horzMove + 'px,'+ vertMove +'px) rotate('+ angle +'deg)' });
         break;
     case "s":
         // spaceship.fadeOut(600);
         // setTimeout(getRandomPos, 600); //might have to change position to fixed
         break;
+    case " ":
+        shoot();
+        break;
     }
 });
+
 
 // function getRandomPos(){
 //   let windowW = $(window).innerWidth();
@@ -46,19 +48,17 @@ $(document).keypress(function(){
 //   spaceship.fadeIn(600).css('top', xPoint);
 //   spaceship.css('left', yPoint);
 // }
-
 function checkOnScreen(){
-  let elem = document.querySelector('.spaceship');
-  let rect = document.querySelector('.spaceship').getBoundingClientRect();
-  let windowW = window.innerWidth + elem.offsetWidth;
-  let windowH = window.innerHeight + elem.offsetHeight;
-  if(rect.top < 0 - elem.offsetHeight){ //spaceship.css isnt working
+  let windowW = window.innerWidth + spaceshipX;
+  let windowH = window.innerHeight + spaceshipY;
+
+  if(rect.top < 0 - spaceshipY){
     console.log("Triggered psoition shift");
     spaceship.css('bottom', '');
     spaceship.css('bottom', -windowH);
   }
 
-  if(rect.left < 0 - elem.offsetWidth){
+  if(rect.left < 0 - spaceshipX){
     console.log("Triggered psoition shift");
     spaceship.css('right', -windowW);
   }
@@ -73,6 +73,21 @@ function checkOnScreen(){
     spaceship.css('left', windowW);
   }
 }
+
+function shoot() {
+    $('body').append($('<div>').addClass('bullet').css({'left': rect.left,'top': rect.top + (spaceshipY/1.5)}));
+}
+let horzMoveBullet = 0;
+let vertMoveBullet = 0;
+function update() {
+    $('.bullet').each(function() {
+        let deg = angle * radian;
+        horzMoveBullet += (Math.sin(deg) * hypotenuse);
+        vertMoveBullet -= (Math.cos(deg) * hypotenuse);
+        $(this).css({'transform': 'translate('+ horzMoveBullet + 'px,'+ vertMoveBullet +'px)'});
+    });
+}
+setInterval(update, 30);
 
 function move(){
   let deg = angle * radian;
